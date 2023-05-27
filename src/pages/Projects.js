c3d750b2e7095456fa058b831ed9803c0699c2e9
Alfred Ngwayah <an017@client1.starzit.com>
2023-05-27 06:37:52 +0000

    Add back button

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Card from '../components/UI/Card';
 import PracticeProjects from '../components/practiceProjects/PracticeProjects';
+import { Link } from 'react-router-dom';
 
 const Projects = () => {
   return (
     <Card>
+      <Link to="/" className="back__btn">
+        <i class="las la-arrow-left"></i>
+      </Link>
       <PracticeProjects />
     </Card>
   );
